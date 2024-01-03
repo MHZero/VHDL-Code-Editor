8e97d90657666c7f3b64ce48e9a3674305fabad4
Haris Khalid <hariskhalidophs2020@gmail.com>
2024-01-02 19:57:46 -0500

    stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+// Get the popup element
+const popup = document.getElementById('popup');
+
+// Function to open the popup
+function openPopup() {
+    popup.style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+    popup.style.display = 'none';
+}
+
+openPopup();
